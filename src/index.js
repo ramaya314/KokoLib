bc24761ace4b83d2ccbd9904802ee0f4cce8aeb8
ramaya314 <ramaya@iPearBook-5.local>
2017-09-30 21:14:27 -0400

    still figuring shit out

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-export NewsLetterSignupForm from './components/NewsLetterSignupForm';
-export TwitterTimeline from './components/TwitterTimeline';
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import NewsLetterSignupForm from './components/NewsLetterSignupForm';
+
+ReactDOM.render(
+	<NewsLetterSignupForm />,
+  document.getElementById('root')
+);
+*/
+
+import NewsLetterSignupForm from './components/NewsLetterSignupForm';
+import TwitterTimeline from './components/TwitterTimeline';
+
+module.exports = {
+  NewsLetterSignupForm,
+  TwitterTimeline
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './build',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
