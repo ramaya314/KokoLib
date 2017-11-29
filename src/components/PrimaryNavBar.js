9a68250919154bee60d9501c3386c754624a5ec5
Ricardo Amaya <ricdamayar@gmail.com>
2017-11-28 22:01:24 -0500

    small changes

--- src/components/PrimaryNavBar.js
+++ src/components/PrimaryNavBar.js
@@ -11,8 +11,6 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 class PrimaryNavBarCSS extends React.PureComponent {
 	render() {
 
-		console.log(this.props.theme);
-
 		return(
 			<div dangerouslySetInnerHTML={{
 			__html: `
